d30edff8b607ca1993597a387b9fbee7853b65e3
Aaron <yachius86@gmail.com>
2019-11-16 12:17:22 -0500

    Beginning development of VPT on an Arduino UNO R3 with an EasyVR3 shield and a BluefuitLE shield

---
+++ vocal-page-turner-uno/bluetooth_hid_keyboard.h
@@ -0,0 +1,19 @@
+/*
+  nano_33_board_utilities.h - Library for controlling board functions of an Arduino Nano 33 BLE Sense
+  Created by Aaron Cohen, November 8, 2019.
+  Released into the public domain.
+*/
+#ifndef bluetoothHIDKeyboard_H
+#define bluetoothHIDKeyboard_H
+
+#include "Arduino.h"
+
+class bluetoothHIDKeyboard {
+  
+  public:
+    bluetoothHIDKeyboard() {
+      
+    };
+};
+
+#endif
---
+++ vocal-page-turner-uno/vocal-page-turner-uno.ino
@@ -0,0 +1,21 @@
+/*
+  Vocal Page Turner
+
+  Use the Arduino Nano 33 BLE Sense to create a bluetooth HID that emits keystrokes based on voice commands using the built-in bluetooth and microphone.
+*/
+
+
+#include "bluetooth_hid_keyboard.h"
+#include "easyvr_3.h"
+
+#include "Arduino.h"
+
+void setup()
+{
+
+}
+
+void loop()
+{
+  
+}
