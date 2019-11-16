b076b8844c156322b35e0d6a17fcd4ef31a6d98d
Aaron Cohen <57534662+yachius86@users.noreply.github.com>
2019-11-16 14:57:05 -0500

    Added the bluetooth code and config

--- vocal-page-turner-uno/bluetooth_hid_keyboard.h
+++ vocal-page-turner-uno/bluetooth_hid_keyboard.h
@@ -1,18 +1,158 @@
 /*
-  nano_33_board_utilities.h - Library for controlling board functions of an Arduino Nano 33 BLE Sense
-  Created by Aaron Cohen, November 8, 2019.
-  Released into the public domain.
+  Send HID (keyboard/mouse/etc) data via BLE
 */
+
 #ifndef bluetoothHIDKeyboard_H
 #define bluetoothHIDKeyboard_H
 
 #include "Arduino.h"
+#include <SPI.h>
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
+#include "Adafruit_BluefruitLE_UART.h"
+
+#include "BluefruitConfig.h"
+
+#if SOFTWARE_SERIAL_AVAILABLE
+  #include <SoftwareSerial.h>
+#endif
+
+/*=========================================================================
+    APPLICATION SETTINGS
+
+    FACTORYRESET_ENABLE       Perform a factory reset when running this sketch
+    MINIMUM_FIRMWARE_VERSION  Minimum firmware version to have some new features
+    -----------------------------------------------------------------------*/
+    #define FACTORYRESET_ENABLE         0
+    #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
+/*=========================================================================*/
+
+Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
 
 class bluetoothHIDKeyboard {
   
   public:
-    bluetoothHIDKeyboard() {
-      
+    
+    void error(const __FlashStringHelper*err) {
+      Serial.println(err);
+      while (1);
+    };
+
+    void init_hid(void)
+    { 
+      /* Initialise the module */
+      Serial.print(F("Initialising the Bluefruit LE module: "));
+    
+      if ( !ble.begin(VERBOSE_MODE) )
+      {
+        error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+      }
+      Serial.println( F("OK!") );
+    
+      if ( FACTORYRESET_ENABLE )
+      {
+        /* Perform a factory reset to make sure everything is in a known state */
+        Serial.println(F("Performing a factory reset: "));
+        if ( ! ble.factoryReset() ){
+          error(F("Couldn't factory reset"));
+        }
+      }
+    
+      /* Disable command echo from Bluefruit */
+      ble.echo(false);
+    
+      Serial.println("Requesting Bluefruit info:");
+      /* Print Bluefruit information */
+      ble.info();
+    
+      /* Change the device name to make it easier to find */
+      Serial.println(F("Setting device name to 'Vocal Page Turner': "));
+      if (! ble.sendCommandCheckOK(F( "AT+GAPDEVNAME=Vocal Page Turner" )) ) {
+        error(F("Could not set device name?"));
+      }
+    
+      /* Enable HID Service */
+      Serial.println(F("Enable HID Service (including Keyboard): "));
+      if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) )
+      {
+        if ( !ble.sendCommandCheckOK(F( "AT+BleHIDEn=On" ))) {
+          error(F("Could not enable Keyboard"));
+        }
+      }else
+      {
+        if (! ble.sendCommandCheckOK(F( "AT+BleKeyboardEn=On"  ))) {
+          error(F("Could not enable Keyboard"));
+        }
+      }
+    
+      /* Add or remove service requires a reset */
+      Serial.println(F("Performing a SW reset (service changes require a reset): "));
+      if (! ble.reset() ) {
+        error(F("Couldn't reset??"));
+      }
+    
+      Serial.println();
+      Serial.println(F("Go to your phone's Bluetooth settings to pair your device"));
+      Serial.println(F("then open an application that accepts keyboard input"));
+    
+      Serial.println();
+      Serial.println(F("Enter the character(s) to send:"));
+      Serial.println(F("- \\r for Enter"));
+      Serial.println(F("- \\n for newline"));
+      Serial.println(F("- \\t for tab"));
+      Serial.println(F("- \\b for backspace"));
+    
+      Serial.println();
+    };
+
+    /**************************************************************************/
+    /*!
+        @brief  Constantly poll for new command or response data
+    */
+    /**************************************************************************/
+    void poll_ble(void)
+    {
+      // Display prompt
+      Serial.print(F("keyboard > "));
+    
+      // Check for user input and echo it back if anything was found
+      char keys[BUFSIZE+1];
+      getUserInput(keys, BUFSIZE);
+    
+      Serial.print("\nSending ");
+      Serial.println(keys);
+    
+      ble.print("AT+BleKeyboard=");
+      ble.println(keys);
+    
+      if( ble.waitForOK() )
+      {
+        Serial.println( F("OK!") );
+      }else
+      {
+        Serial.println( F("FAILED!") );
+      }
+    };
+
+    /**************************************************************************/
+    /*!
+        @brief  Checks for user input (via the Serial Monitor)
+    */
+    /**************************************************************************/
+    void getUserInput(char buffer[], uint8_t maxSize)
+    {
+      memset(buffer, 0, maxSize);
+      while( Serial.available() == 0 ) {
+        delay(1);
+      }
+    
+      uint8_t count=0;
+    
+      do
+      {
+        count += Serial.readBytes(buffer+count, maxSize);
+        delay(2);
+      } while( (count < maxSize) && !(Serial.available() == 0) );
     };
 };
 
