1378aa97a11f6c034ce9594b567f8ad3fc17d87a
Aaron Cohen <57534662+yachius86@users.noreply.github.com>
2019-11-16 14:06:18 -0500

    Setup the basic structure of the main loop
    Initialize EasyVR and wait for a command
    Structure to send commands to the keyboard when EasyVR returns a recognition

--- vocal-page-turner-uno/vocal-page-turner-uno.ino
+++ vocal-page-turner-uno/vocal-page-turner-uno.ino
@@ -4,18 +4,41 @@
   Use the Arduino Nano 33 BLE Sense to create a bluetooth HID that emits keystrokes based on voice commands using the built-in bluetooth and microphone.
 */
 
-
+#include "Arduino.h"
 #include "bluetooth_hid_keyboard.h"
 #include "easyvr_3.h"
 
-#include "Arduino.h"
+EasyVR3 vrUtil;
 
 void setup()
 {
-
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println();
+  Serial.println("Initializing VPT");
+  vrUtil.init_vr();
+  Serial.println("VPT Ready");
 }
 
 void loop()
 {
-  
+  int8_t recognized_command_num = vrUtil.vr_listen();
+
+  switch (recognized_command_num) {
+    case 0:
+      //next page
+      Serial.println("Recognized command 0, sending command to keyboard");
+      break;
+    case 1:
+      //previous page
+      Serial.println("Recognized command 1, sending command to keyboard");
+      break;
+    case -1:
+      //no recognized command before the timeout or an error
+      break;
+    default:
+      Serial.println("Unrecognized command"); 
+  }
 }
