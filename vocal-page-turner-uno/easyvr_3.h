f4bac1c11241a27cd6db574804886b6735084998
Aaron Cohen <57534662+yachius86@users.noreply.github.com>
2019-11-16 14:57:30 -0500

    Cleaned up the EasyVR class
    Just removing unused functionality

--- vocal-page-turner-uno/easyvr_3.h
+++ vocal-page-turner-uno/easyvr_3.h
@@ -1,48 +1,14 @@
 /**
-  EasyVR Tester
+  EasyVR
 
-  Dump contents of attached EasyVR module
-  and exercise it with playback and recognition.
-
-  Serial monitor can be used to send a few basic commands:
-  '?' - display the module setup
-  'l' - cycles through available languages
-  'c' - cycles through available command groups
-  'b' - cycles through built-in word sets
-  'g' - cycles through custom grammars
-  's123' - play back sound 123 if available (or beep)
-  'd0123456789ABCD*#' - dials the specified number ('_' is dial tone)
-  'k' - starts detection of tokens
-  '4' or '8' - sets token length to 4 or 8 bits
-  'n123' - play back token 123 (not checked for validity)
   'm1' - set mic distance to HEADSET
   'm2' - set mic distance to ARMS_LENGTH (default)
   'm3' - set mic distance to FAR_MIC
-  'w' - enter sleep mode without audio wakeup (any command interrupts)
-  'ww' - enter sleep mode with "whistle" wakeup
-  'w2' - enter sleep mode with "double-clap" wakeup
-  'w3' - enter sleep mode with "triple-clap" wakeup
-  'wl' - enter sleep mode with "loud-sound" wakeup
-  'r12' - record message 12 if empty
-  'p12' - play back message 12 if recorded
-  'e12' - erase message 12
 
   With EasyVR Shield, the green LED is ON while the module
   is listening (using pin IO1 of EasyVR).
   Successful recognition is acknowledged with a beep.
   Details are displayed on the serial monitor window.
-
-**
-  Example code for the EasyVR library v1.10.1
-  Written in 2017 by RoboTech srl for VeeaR <http:://www.veear.eu>
-
-  To the extent possible under law, the author(s) have dedicated all
-  copyright and related and neighboring rights to this software to the
-  public domain worldwide. This software is distributed without any warranty.
-
-  You should have received a copy of the CC0 Public Domain Dedication
-  along with this software.
-  If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 */
 
 #ifndef easyVR3_H
@@ -215,31 +181,6 @@ class EasyVR3 {
         }
       }
     
-      pcSerial.print(F("Recorded messages:"));
-      if (easyvr.getID() >= EasyVR::EASYVR3_1)
-      {
-        pcSerial.println();
-        for (int8_t idx = 0; idx < 32; ++idx)
-        {
-          int8_t bits = -1; int32_t len = -1;
-        easyvr.dumpMessage(idx, bits, len);
-          if ((bits == 0) && (len == 0))
-            continue; // skip empty
-          pcSerial.print(idx);
-          pcSerial.print(F(" = "));
-          if (bits < 0 || len < 0)
-            pcSerial.println(F(" has errors"));
-          else
-          {
-            pcSerial.print(bits);
-            pcSerial.print(F(" bits, size "));
-            pcSerial.println(len);
-          }
-        }
-      }
-      else
-        pcSerial.println(F("n/a"));
-    
       easyvr.setTimeout(30);
       lang = EasyVR::ENGLISH;
       easyvr.setLanguage(lang);
@@ -248,55 +189,6 @@ class EasyVR3 {
       easyvr.setCommandLatency(EasyVR::MODE_FAST);
     
       int16_t count = 0;
-    
-      pcSerial.print(F("Sound table: "));
-      if (easyvr.dumpSoundTable(name, count))
-      {
-        pcSerial.println(name);
-        pcSerial.print(F("Sound entries: "));
-        pcSerial.println(count);
-      }
-      else
-        pcSerial.println(F("n/a"));
-    
-      pcSerial.print(F("Custom Grammars: "));
-      grammars = easyvr.getGrammarsCount();
-      if (grammars > 4)
-      {
-        pcSerial.println(grammars - 4);
-        for (set = 4; set < grammars; ++set)
-        {
-          pcSerial.print(F("Grammar "));
-          pcSerial.print(set);
-    
-          uint8_t flags, num;
-          if (easyvr.dumpGrammar(set, flags, num))
-          {
-            pcSerial.print(F(" has "));
-            pcSerial.print(num);
-            if (flags & EasyVR::GF_TRIGGER)
-              pcSerial.println(F(" trigger"));
-            else
-              pcSerial.println(F(" command(s)"));
-          }
-          else
-          {
-            pcSerial.println(F(" error"));
-            continue;
-          }
-    
-          for (int8_t idx = 0; idx < num; ++idx)
-          {
-            pcSerial.print(idx);
-            pcSerial.print(F(" = "));
-            if (!easyvr.getNextWordLabel(name))
-              break;
-            pcSerial.println(name);
-          }
-        }
-      }
-      else
-        pcSerial.println(F("n/a"));
     
       if (easyvr.getGroupMask(mask))
       {
@@ -391,116 +283,6 @@ class EasyVR3 {
         pcSerial.println(F("Forced wake-up!"));
         return true;
       }
-      if (rx == 'l')
-      {
-        easyvr.stop();
-        lang++;
-        if (easyvr.setLanguage(lang) || easyvr.setLanguage(lang = 0))
-        {
-          pcSerial.print(F("Language set to "));
-          pcSerial.println(lang);
-        }
-        else
-          pcSerial.println(F("Error while setting language!"));
-      }
-      if (rx == 'b')
-      {
-        useTokens = false;
-        useCommands = false;
-        useGrammars = true;
-        set++;
-        if (set > 3)
-          set = 0;
-      }
-      if (rx == 'g' && grammars > 4)
-      {
-        useTokens = false;
-        useCommands = false;
-        useGrammars = true;
-        set++;
-        if (set >= grammars)
-          set = 4;
-      }
-      if (rx == 'c')
-      {
-        useTokens = false;
-        useCommands = true;
-        useGrammars = false;
-        do
-        {
-          group++;
-          if (group > EasyVR::PASSWORD)
-            group = 0;
-        }
-        while (!((mask >> group) & 1));
-      }
-      if (rx == 'k')
-      {
-        useTokens = true;
-        useCommands = false;
-        useGrammars = false;
-      }
-      if (rx == '4')
-      {
-        bits = 4;
-      }
-      if (rx == '8')
-      {
-        bits = 8;
-      }
-      if (rx == 'n')
-      {
-        int16_t num = readNum();
-        pcSerial.print(F("Play token "));
-        pcSerial.println(num);
-        easyvr.stop();
-        easyvr.sendToken(bits, num);
-      }
-      if (rx == 's')
-      {
-        int16_t num = 0;
-        delay(5);
-        while ((rx = pcSerial.read()) >= 0)
-        {
-          delay(5);
-          if (isdigit(rx))
-            num = num * 10 + (rx - '0');
-          else
-            break;
-        }
-        pcSerial.print(F("Play sound "));
-        pcSerial.println(num);
-        easyvr.stop();
-        easyvr.playSound(num, EasyVR::VOL_DOUBLE);
-      }
-      if (rx == 'd')
-      {
-        easyvr.stop();
-        pcSerial.println(F("Play tones:"));
-        int16_t num = 0;
-        delay(5);
-        while ((rx = pcSerial.read()) >= 0)
-        {
-          delay(5);
-          if (isdigit(rx))
-            num = rx - '0';
-          else if (rx == '*')
-            num = 10;
-          else if (rx == '#')
-            num = 11;
-          else if (rx >= 'A' && rx <= 'D')
-            num = rx - 'A';
-          else if (rx == '_')
-            num = -1;
-          else
-            break;
-          pcSerial.print(num);
-          if (easyvr.playPhoneTone(num, 3))
-            pcSerial.println(F(" OK"));
-          else
-            pcSerial.println(F(" ERR"));
-        }
-      }
       if (rx == 'm')
       {
         int16_t num = readNum();
@@ -509,70 +291,6 @@ class EasyVR3 {
         easyvr.stop();
         easyvr.setMicDistance(num);
       }
-      if (rx == 'w')
-      {
-        int8_t mode = 0;
-        delay(5);
-        while ((rx = pcSerial.read()) >= 0)
-        {
-          delay(5);
-          if (rx == 'w')
-            mode = EasyVR::WAKE_ON_WHISTLE;
-          if (rx == '2')
-            mode = EasyVR::WAKE_ON_2CLAPS;
-          if (rx == '3')
-            mode = EasyVR::WAKE_ON_3CLAPS;
-          if (rx == 'l')
-            mode = EasyVR::WAKE_ON_LOUDSOUND;
-        }
-        pcSerial.print(F("Sleep mode "));
-        pcSerial.println(mode);
-        easyvr.stop();
-        if (easyvr.getID() < EasyVR::EASYVR3)
-          easyvr.setPinOutput(EasyVR::IO1, LOW);  // Shield 2.0 LED off
-        isSleeping = easyvr.sleep(mode);
-        return true;
-      }
-      if (rx == 'r')
-      {
-        int8_t num = readNum();
-        pcSerial.print(F("Record (5 seconds) message "));
-        pcSerial.println(num);
-        easyvr.stop();
-        easyvr.recordMessageAsync(num, 8, 5);
-        useTokens = false;
-        useCommands = false;
-        useGrammars = false;
-        isBusy = true;
-        return true;
-      }
-      if (rx == 'p')
-      {
-        int8_t num = readNum();
-        pcSerial.print(F("Play message "));
-        pcSerial.println(num);
-        easyvr.stop();
-        easyvr.playMessageAsync(num, EasyVR::SPEED_NORMAL, EasyVR::ATTEN_NONE);
-        useTokens = false;
-        useCommands = false;
-        useGrammars = false;
-        isBusy = true;
-        return true;
-      }
-      if (rx == 'e')
-      {
-        int8_t num = readNum();
-        pcSerial.print(F("Erase message "));
-        pcSerial.println(num);
-        easyvr.stop();
-        easyvr.eraseMessageAsync(num);
-        useTokens = false;
-        useCommands = false;
-        useGrammars = false;
-        isBusy = true;
-        return true;
-      }
-    
       if (rx >= 0)
       {
         easyvr.stop();
@@ -592,24 +310,11 @@ class EasyVR3 {
         if (easyvr.getID() < EasyVR::EASYVR3)
           easyvr.setPinOutput(EasyVR::IO1, HIGH); // Shield 2.0 LED on (listening)
     
-        if (useTokens)
-        {
-          pcSerial.print(F("Detect a "));
-          pcSerial.print(bits);
-          pcSerial.println(F(" bit token ..."));
-          easyvr.detectToken(bits, EasyVR::REJECTION_AVG, 0);
-        }
-        else if (useCommands)
+        if (useCommands)
         {
           pcSerial.print(F("Say a command in Group "));
           pcSerial.println(group);
           easyvr.recognizeCommand(group);
-        }
-        else if (useGrammars)
-        {
-          pcSerial.print(F("Say a word in Wordset "));
-          pcSerial.println(set);
-          easyvr.recognizeWord(set);
         }
       }
       do
@@ -698,13 +403,6 @@ class EasyVR3 {
           }
           else
             pcSerial.println();
-          // ok, let's try another group
-//          do
-//          {
-//            group++;
-//            if (group > EasyVR::PASSWORD)
-//              group = 0;
-//          }
           while (!((mask >> group) & 1));
           easyvr.playSound(0, EasyVR::VOL_FULL);
         }
@@ -724,8 +422,6 @@ class EasyVR3 {
       }
       return recognized_command_num;
     };
-
-
 };
 
 #endif
